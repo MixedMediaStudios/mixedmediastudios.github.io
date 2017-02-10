119720cdc2e062c8c7d97205c1c0d46c1487e95c
Owen Walters <owenwalters.net@gmail.com>
2017-02-10 08:46:14 -0500

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+var width = $(window).width()
+if ( width > 899 )
+$( function(){
+  $.stellar({
+    responsive: true,
+  verticalScrolling: true,
+  horizontalOffset: 0,
+  verticalOffset: 0,
+  positionProperty: 'transform',
+  hideDistantElements: false
+  });
+})
+{% for author_entry in site.data.authors %}{% assign author = author_entry[1] %}
+$('.{{ author.id }}').hovercard({
+  detailsHTML: "<img src=\"{{ author.picture }}\" width=\"200px\" alt=\"Picture of {{ author.name }}\"><br />{{ author.name }} is the {{ author.role }} at {{ site.title }}. {% if author.github %}<br/> Github profile: <a href=\"{{ author.github }}\">{{ author.github }}</a>{% endif %}"
+})
+{% endfor %}
+$('.mms').hovercard({
+  detailsHTML: "<img src=\"{{ site.icon }}\" width=\"200px\" alt=\"Picture of {{ site.title }}\"><br /> The official blog profile for {{ site.title }}."
+})
